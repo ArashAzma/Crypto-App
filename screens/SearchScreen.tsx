091169f43e5da88420a19170f9a6fda18a8789de
ArashAzma <arashazma3@gmail.com>
2023-11-25 11:48:06 +0330

    fix: fixed filtering coins types

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,27 +1,38 @@
 import {MaterialCommunityIcons} from '@expo/vector-icons';
-import {useObservable} from '@legendapp/state/react';
+import {ObservableBaseFns, ObservableComputed} from '@legendapp/state';
+import {Computed, useComputed, useObservable} from '@legendapp/state/react';
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
+import {keysOf} from './../utils/HelperFunctions';
+import CoinItem from '../components/CoinItem';
 import TextField from '../components/TextField';
 import {state$} from '../GlobalState';
 import {screenWidth} from '../utils/Dimensions';
 import {BLACK, DARK_BLUE, WHITE} from '../utils/Theme';
-import {type Coin} from '../utils/Types';
+import {CoinName, type Coin} from '../utils/Types';
 
 function SearchScreen() {
-  const search$ = useObservable({text: '', searchedCoins: [] as Coin[]});
+  const search$ = useObservable({
+    text: '',
+    searchedCoins: [] as ObservableBaseFns<Coin>[],
+  });
+  const coins$: ObservableComputed<Coin[]> = useComputed(() => {
+    const coinToPriceMap = state$.coinToPriceMap.get();
+    return keysOf(coinToPriceMap).map((coinName) => ({
+      name: coinName as CoinName,
+      price: Number(coinToPriceMap[coinName]),
+    }));
+  });
 
   function submitSearch() {
     const searchInput = search$.text.peek();
-    console.log('searchInput: ', searchInput);
-    console.log('peek', state$.coinToPriceMap.peek());
-    const matchingCoins = Object.keys(state$.coinToPriceMap.peek()).filter(
-      (coin) => coin.toLowerCase().includes(searchInput.toLowerCase()),
+    coins$.peek();
+    const matchingCoins = coins$.filter((coin) =>
+      coin.peek().name.toLowerCase().includes(searchInput.toLowerCase()),
     );
-
-    console.log('Matching Coins:', matchingCoins);
+    search$.searchedCoins.set(matchingCoins);
   }
   return (
     <View style={styles.container}>
@@ -31,6 +42,12 @@ function SearchScreen() {
           <MaterialCommunityIcons name='magnify' size={26} color={WHITE} />
         </TouchableOpacity>
       </View>
+      <Computed>
+        <FlatList
+          data={search$.searchedCoins.get()}
+          renderItem={({item}) => <CoinItem coin$={item} />}
+        />
+      </Computed>
     </View>
   );
 }
@@ -47,8 +64,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'flex-start',
     width: screenWidth * 0.9,
-    height: 45,
+    height: 55,
     borderRadius: 14,
+    marginVertical: 40,
   },
   searchContainer: {
     justifyContent: 'center',

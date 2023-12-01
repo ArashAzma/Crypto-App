6efd8bddca5b8ca46a44085d0d5765a9d8b8fc57
ArashAzma <arashazma3@gmail.com>
2023-12-01 17:48:50 +0330

    feat: add persistence using mmkv

--- GlobalState.tsx
+++ GlobalState.tsx
@@ -1,8 +1,17 @@
 import {observable} from '@legendapp/state';
+import {
+  configureObservablePersistence,
+  persistObservable,
+} from '@legendapp/state/persist';
+import {ObservablePersistMMKV} from '@legendapp/state/persist-plugins/mmkv';
 
 import {type Gender, type Coin, type CoinName} from './utils/Types';
 
 type PinnedCoin = {name: CoinName | null; priceHistory: number[]};
+
+configureObservablePersistence({
+  pluginLocal: ObservablePersistMMKV,
+});
 
 export const state$ = observable({
   coins: [] as Coin[],
@@ -20,4 +29,8 @@ export const settings$ = observable({
     gender: undefined as Gender,
     imageUrl: 'https://i.stack.imgur.com/34AD2.jpg',
   },
+});
+
+persistObservable(settings$, {
+  local: 'settings',
 });
